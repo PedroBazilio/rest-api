5e387d70be5a62d062065fbe2f344d9a2a460355
pedro.bazilio <pedro.bazilio@owse.com.br>
2023-07-24 09:20:42 -0300

    adicionando boas vindo ao localhost:8080

---
+++ src/main/java/com/pedro/restapi/rest/HelloResource.java
@@ -0,0 +1,24 @@
+package com.pedro.restapi.rest;
+
+import com.pedro.restapi.domain.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HelloResource {
+
+
+    private final Logger log = LoggerFactory.getLogger(HelloResource.class);
+
+
+    @GetMapping("")
+    public ResponseEntity<String> getPendingTasks() {
+        log.info("Welcome to the project");
+        return ResponseEntity.ok("Boas vindas ao projeto");
+    }
+}
